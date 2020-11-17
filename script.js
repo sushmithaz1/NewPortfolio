dee44f2132974f8a13df741a5b86fd7808cb77cc
sushmitha R <59172301+sushmithaz1@users.noreply.github.com>
2020-11-18 00:55:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: ["Website Developer", "Backend Developer", "Android Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Website Developer", "Backend Developer", "Android Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
